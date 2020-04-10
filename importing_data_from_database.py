30f641d030498a54949f4db400c5714f940dcb23
Vagif <30445708+vagi@users.noreply.github.com>
2020-04-10 18:04:45 +0300

    Create importing_data_from_database.py

---
+++ importing_data_from_database.py
@@ -0,0 +1,23 @@
+# Importing data from database using SQLite3
+# A good practice is to save/commit your transactions using the commit() function even if you are only reading the data.
+
+import pandas as pd
+import sqlite3
+
+# open engine connection
+con=sqlite3.connect('./files/sample_test.db')
+
+# create a cursor object
+cur = con.cursor()
+
+# Perform query: rs
+rs = cur.execute('select * from TEST')
+
+# Save results of the query to DataFrame: df
+df = pd.DataFrame(rs.fetchall())
+
+# Close connection
+con.commit()
+
+# Print head of DataFrame df
+df
