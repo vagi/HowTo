1aaa0acf90a7003dc108b7c18fd4438203fff65e
Vagif <30445708+vagi@users.noreply.github.com>
2020-04-10 17:41:53 +0300

    Update reading_csv_files.py

--- reading_csv_files.py
+++ reading_csv_files.py
@@ -8,3 +8,14 @@ df = pd.read_csv(r'./files/data.csv')
 
 # display DataFrame
 df
+
+
+"""
+But CSV can run into problems if the values contain commas. 
+This can be overcome by using different delimiters to separate information in the file, 
+like ‘\t’ or ‘;’, etc. These can also be imported with the read_csv() function by specifying 
+the delimiter in the parameter value as shown below while reading a TSV (Tab Separated Values) file:
+""" 
+
+df = pd.read_csv(r'./files/file.txt',delimiter='\t')
+df
